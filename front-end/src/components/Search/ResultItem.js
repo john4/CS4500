8048f642d0476b96e8f8df8c428479e725645d30
Ben Campbell <bencampbell@Bens-MacBook-Pro-2.local>
2018-02-21 19:42:04 -0500

    I forgot to add title to the search results [TEAM42-54]

--- front-end/src/components/Search/ResultItem.js
+++ front-end/src/components/Search/ResultItem.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class ResultItem extends Component {
     render() {
       return <li>
-        <h1>{this.props.trackName}</h1>
+        <h3>{this.props.title}</h3>
         <img src={this.props.posterSrc} width="100px"/>
         <div>{this.props.overview}</div>
         </li>;

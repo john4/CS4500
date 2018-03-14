cc63bf1e951342132b80605627143e50a9e02e08
arianna2028 <arianna2028@outlook.com>
2018-03-14 17:22:24 -0400

    Satisfy pylint standards (mostly)

--- cs4500-spring2018-team42-flask/tests/movie_tests.py
+++ cs4500-spring2018-team42-flask/tests/movie_tests.py
@@ -2,87 +2,81 @@
 import json
 import unittest
 
-from app import app, db
+from app import APP, DB
 from models import Movie
 
 class GetMovieTests(unittest.TestCase):
-	
-	def setUp(self):
-		app.config['TESTING'] = True
-		app.config['WTF_CSRF_ENABLED'] = False
-		app.config['DEBUG'] = False
-		self.app = app.test_client()
-		self.assertEqual(app.debug, False)
-		
-		db.Movie.insert_one({ 
-				"_id" : 3924, 
-				"adult" : False, 
-				"original_title" : "Blondie", 
-				"popularity" : 1.274044, 
-				"video" : False
-			})
-			
-		self.m = Movie()
-	
-	def tearDown(self):
-		db.Movie.delete_many({})
-		self.m = None
 
-	# Tests ##################################
-	
-	def test_get_1_movie(self):
-		value = self.m.getMovies(1)
-				
-		self.assertEqual(1, value.count())
-		
-	def test_get_neg1_movie(self):
-		value = self.m.getMovies(-1)
-						
-		self.assertEqual(1, value.count())
-		
-	def test_get_more_movies_than_in_db(self):
-		value = self.m.getMovies(2)
-		
-		self.assertEqual(1, value.count())
-		
-	def test_api_route_positive(self):
-		value = self.app.get('/movies/1')
-		
-		result = json.loads(value.get_data(as_text=True))
-						
-		self.assertEqual(1, len(result))
-		self.assertEqual(3924, result[0]["_id"])
-		
+    def setUp(self):
+        APP.config['TESTING'] = True
+        APP.config['WTF_CSRF_ENABLED'] = False
+        APP.config['DEBUG'] = False
+        self.app = APP.test_client()
+        self.assertEqual(APP.debug, False)
+
+        DB.Movie.insert_one({
+                "_id" : 3924,
+                "adult" : False,
+                "original_title" : "Blondie",
+                "popularity" : 1.274044,
+                "video" : False
+            })
+
+    def tearDown(self):
+        DB.Movie.delete_many({})
+
+    # Tests ##################################
+
+    def test_get_1_movie(self):
+        value = Movie.getMovies(1)
+
+        self.assertEqual(1, value.count())
+
+    def test_get_neg1_movie(self):
+        value = Movie.getMovies(-1)
+
+        self.assertEqual(1, value.count())
+
+    def test_get_more_movies_than_in_DB(self):
+        value = Movie.getMovies(2)
+
+        self.assertEqual(1, value.count())
+
+    def test_api_route_positive(self):
+        value = self.app.get('/movies/1')
+
+        result = json.loads(value.get_data(as_text=True))
+
+        self.assertEqual(1, len(result))
+        self.assertEqual(3924, result[0]["_id"])
+
 
 class GetMovieDetailTests(unittest.TestCase):
 
-	def setUp(self):
-		app.config['TESTING'] = True
-		app.config['WTF_CSRF_ENABLED'] = False
-		app.config['DEBUG'] = False
-		self.app = app.test_client()
-		self.assertEqual(app.debug, False)
-		
-		self.m = Movie()
+    def setUp(self):
+        APP.config['TESTING'] = True
+        APP.config['WTF_CSRF_ENABLED'] = False
+        APP.config['DEBUG'] = False
+        self.app = APP.test_client()
+        self.assertEqual(APP.debug, False)
 
-		
-	def tearDown(self):
-		self.m = None
-		
-	def test_get_movie_8_detail(self):
-		results, response_code = self.m.getMovieDetails(8)
-		
-		self.assertEqual(response_code, 200)
-		self.assertEqual(results["id"], 8)
+    def tearDown(self):
+        pass
 
-	def test_get_illegal_id_movie(self):
-		results, response_code = self.m.getMovieDetails(-1)
-		self.assertEqual(response_code, 404)
-		self.assertEqual(results["status_message"], "The resource you requested could not be found.")
-		
-	def test_get_api_route_positive(self):
-		value = self.app.get('/movies/details/8')
-		
-		result = json.loads(value.get_data(as_text=True))
-				
-		self.assertEqual(8, result["id"])
+    def test_get_movie_8_detail(self):
+        results, response_code = Movie.get_movie_details(8)
+
+        self.assertEqual(response_code, 200)
+        self.assertEqual(results["id"], 8)
+
+    def test_get_illegal_id_movie(self):
+        results, response_code = Movie.get_movie_details(-1)
+        self.assertEqual(response_code, 404)
+        self.assertEqual(results["status_message"], "The resource you requested could not be found.")
+
+    def test_get_api_route_positive(self):
+        value = self.app.get('/movies/details/8')
+
+        result = json.loads(value.get_data(as_text=True))
+
+        self.assertEqual(8, result["id"])

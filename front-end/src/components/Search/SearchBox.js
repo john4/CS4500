511a92a994cbf4c989f467c9859ae24e2eb363f0
Ben Campbell <campbell.b@husky.neu.edu>
2018-03-15 15:58:35 -0400

    TEAM42-99 #comment removed commented styles #ready-for-review

--- front-end/src/components/Search/SearchBox.js
+++ front-end/src/components/Search/SearchBox.js
@@ -17,9 +17,9 @@ class SearchBox extends Component {
     render() {
       return (
         <div className="SearchBox input-group">
-            <input className="SearchBox-text form-control" type="text" ref="query" placeholder="Batman..."/>
+            <input className="form-control" type="text" ref="query" placeholder="Batman..."/>
             <span class="input-group-btn">
-              <button className="SearchBox-submit btn btn-default" type="button" onClick={this.createAjax} value="Search">Search</button>
+              <button className="btn btn-default" type="button" onClick={this.createAjax} value="Search">Search</button>
             </span>
         </div>
       );

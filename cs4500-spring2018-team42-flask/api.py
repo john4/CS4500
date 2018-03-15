730995e3ed2da6980c8129fb1e59d465d3d50a35
arianna2028 <arianna2028@outlook.com>
2018-03-15 17:06:19 -0400

    #comment add api endpoints and architecture for 1-5 ratings on movies

--- cs4500-spring2018-team42-flask/api.py
+++ cs4500-spring2018-team42-flask/api.py
@@ -2,8 +2,7 @@
 
 from flask import json, make_response, request, jsonify
 from app import APP
-from models import User
-from models import Movie
+from models import Movie, User, Review
 
 
 @APP.route('/')
@@ -44,17 +43,39 @@ def login_user():
 
     return make_response(login_result, response_status)
 
-@APP.route('/movies/<int:count>', methods=['GET'])
+@APP.route('/movies/<int:count>/', methods=['GET'])
 def get_movies(count):
     """get a list of movies from the db"""
 
     results = Movie.get_movies(count)
     return make_response(jsonify(list(results)), 200)
 
-@APP.route('/movies/details/<int:movie_id>', methods=['GET'])
+@APP.route('/movies/details/<int:movie_id>/', methods=['GET'])
 def get_movie_details(movie_id):
     """get a movie's details from the db"""
 
     results, response_code = Movie.get_movie_details(movie_id)
 
     return make_response(jsonify(results), response_code)
+
+@APP.route('/movie/<int:movie_id>/review/', methods=['POST'])
+def review_movie(movie_id):
+    """rate a movie from 1-5 stars (add more later)"""
+    #TODO: handle update
+
+    new_review = Review()
+    data = json.loads(request.data)
+
+    new_review.tmdb_id = movie_id
+    new_review.user_email = data.get('user_email')
+    new_review.rating = data.get('rating')
+
+    results, response_code = new_review.create()
+    return make_response(results, response_code)
+
+@APP.route('/movie/<int:movie_id>/rating/', methods=['GET'])
+def get_movie_avg_rating(movie_id):
+    """get the average rating for a movie"""
+
+    results, response_code = Movie.get_average_rating(movie_id)
+    return make_response(results, response_code)

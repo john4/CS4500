78e5812d48ab3b1d41710d3d1146857d5c019f97
John <martin.john@husky.neu.edu>
2018-03-22 16:27:13 -0400

    TEAM42-115 #comment very basic navigation component to help make using the app easier #in-development

---
+++ front-end/src/components/NavBar.js
@@ -0,0 +1,34 @@
+'use es6';
+
+import React, { Component } from 'react';
+import '../App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'react-bootstrap';
+import AccountNav from './User/AccountNav';
+
+class NavBar extends Component {
+
+  render() {
+    const isLoggedIn = true;
+
+    return (
+      <header className="App-header">
+        <div className="row">
+          <div className="col">
+            <h1 className="App-title">Spoiled Tomatillos</h1>
+          </div>
+          <div className="col navbar">
+            <a href="/search">Search</a>
+            <a href="/logout">Logout</a>
+          </div>
+          <div className="col text-right">
+            <AccountNav />
+          </div>
+        </div>
+
+      </header>
+    );
+  }
+}
+
+export default NavBar;

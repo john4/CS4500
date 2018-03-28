ba7b53ea5804b6d2e016d1c7ace246b94dfd6361
loakley <oakley.l@husky.neu.edu>
2018-03-27 23:39:33 -0400

    TEAM42-111-playlist-v1 #comment Fix tabs and camel casing

--- front-end/src/components/Playlist/Genre.js
+++ front-end/src/components/Playlist/Genre.js
@@ -5,36 +5,36 @@ import { ApiWrapper } from '../../ApiWrapper.js';
 
 class Genre extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            genre: ''
-        };
-        this.receiveDetails = this.receiveDetails.bind(this)
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      genre: ''
+    };
+    this.receiveDetails = this.receiveDetails.bind(this)
+  }
 
-    receiveDetails(res) {
-        this.setState({
-            genre: res.data.genre
-        })
-    }
+  receiveDetails(res) {
+    this.setState({
+      genre: res.data.genre
+    })
+  }
 
-    componentWillMount() {
-        ApiWrapper().api().getAccountDetails().then(this.receiveDetails);
-    }
+  componentWillMount() {
+    ApiWrapper().api().getAccountDetails().then(this.receiveDetails);
+  }
 
-    render() {
-        var gen = this.state.genre;
-        if (this.props.name === '') {
-            return "Must be logged in to view recommendations"
-        } else if (gen) {
-            return <div>
-            <h1>Playlist based on your favorite genre, {gen}</h1>
-            <GenrePlaylist genre_id={GENRES[gen]} />
-        </div>
-        }
-        else
-            return "No playlist available for your favorite genre"
+  render() {
+    var gen = this.state.genre;
+    if (this.props.name === '') {
+      return "Must be logged in to view recommendations"
+    } else if (gen) {
+      return <div>
+      <h1>Playlist based on your favorite genre, {gen}</h1>
+      <GenrePlaylist genreID={GENRES[gen]} />
+    </div>
     }
+    else
+      return "No playlist available for your favorite genre"
+  }
 }
 export default Genre;
--- front-end/src/components/Playlist/Playlist.js
+++ front-end/src/components/Playlist/Playlist.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 import Results from '../Search/Results';
 
 class Playlist extends Component {
-    render() {
-        return (
-            <div>
-                <Results searchResults={this.props.playlist} />
-            </div>
-          );
-    } 
+  render() {
+    return (
+      <div>
+        <Results searchResults={this.props.playlist} />
+      </div>
+      );
+  } 
 }
 export default Playlist;

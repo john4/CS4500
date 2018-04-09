a150fecaafcf443dc14bf1fef7d714be09451fbb
loakley <oakley.l@husky.neu.edu>
2018-04-08 21:11:42 -0400

    TEAM42-132 #comment Add config values

---
+++ cs4500-spring2018-team42-flask/config.py
@@ -0,0 +1,4 @@
+FLASK_DB_URI='mongodb://%s:%s@ec2-54-197-196-20.compute-1.amazonaws.com'
+FLASK_DB_USERNAME='admin'
+FLASK_DB_SECRET='team42root'
+FLASK_TMDB_API_KEY='020a1282ad51b08df67da919fca9f44e'

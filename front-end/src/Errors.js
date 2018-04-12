46dacc8a01eafc27a1d71560cc4b8bf2799191d2
loakley <oakley.l@husky.neu.edu>
2018-04-11 22:10:45 -0400

    TEAM42-164 #comment Fix typo

--- front-end/src/Errors.js
+++ front-end/src/Errors.js
@@ -4,7 +4,7 @@ export const NO_MOVIE_RESULTS = "Sorry, no movies match your query. Try narrowin
 export const NO_USER_RESULTS = "Sorry, no users match your query. Try narrowing your search."
 export const NO_NOTIFS = "No new notifications."
 export const NO_FOLLOWERS = <span>Oops, you have no followers. Try <a href="/">rating recommended movies</a> and <a href="user-search">following other users</a> to gain visibility.</span>
-export const NO_FOLLOWING = <span>Oops, you aren't following anyone. <a href="/user-search">Search users</a> or <a href="/">look at reviews on recommended movies</a> to find people to follow.</span>
+export const NO_FOLLOWING = <span>Oops, you are not following anyone. <a href="/user-search">Search users</a> or <a href="/">look at reviews on recommended movies</a> to find people to follow.</span>
 export const NO_FOLLOWERS_OTHER = "This user has no followers"
 export const NO_FOLLOWING_OTHER = "This user is not following anyone"
 export const NO_RECENT_REVIEWS = "No recent reviews"

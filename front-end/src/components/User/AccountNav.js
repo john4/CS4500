809f4e81021c17922d5cecc1d9cc8f99962dfaf5
Peter Battinelli <battinelli.p@husky.neu.edu>
2018-04-11 22:38:31 -0400

    Revert "Forgot this guy. [TEAM42-179]"
    This reverts commit 289ea484a12ff9d0ad633712e1460b41f68650b3.

--- front-end/src/components/User/AccountNav.js
+++ front-end/src/components/User/AccountNav.js
@@ -13,29 +13,21 @@ class Account extends Component {
     };
   }
   
-  renderUserImage() {
-      if(this.props.photoUrl){
-        return (<img src={this.props.photoUrl} class="" />);
-      } else {
-        return (<span className="fa-stack">
-                    <i class="fa fa-circle fa-stack-2x" />
-                    <i class="fas fa-user fa-stack-1x fa-inverse" />
-                </span>);
-      }
-  }
-  
   render() {
-    const photoUrl = this.props.photoUrl;
+    const username = this.props.username;
     const { menuOpen } = this.state;
     const openClass = menuOpen ? "show" : "";
 
-    if (photoUrl) {
+    if (username) {
       return (
         <div className={`AccountNav-dropdown dropdown  ${openClass}`}>
           
-          <a onClick={() => this.setState({menuOpen: !menuOpen})} href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded={menuOpen}>
-            {this.renderUserImage()}
-          </a>
+            <a onClick={() => this.setState({menuOpen: !menuOpen})} href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded={menuOpen}>
+                <span className="fa-stack">
+                    <i class="fa fa-circle fa-stack-2x" />
+                    <i class="fas fa-user fa-stack-1x fa-inverse" />
+                </span>
+            </a>
           <ul className={`dropdown-menu dropdown-menu-right ${openClass}`} aria-labelledby="dropdownMenuLink">
             <a className="dropdown-item" href="/user/profile">Profile</a>
             <a className="dropdown-item" href="/logout">Log out</a>
